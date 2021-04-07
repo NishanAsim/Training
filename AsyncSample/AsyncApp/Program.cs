981869674f59c4cc866ec9b623518a1128795bd7
NishanAsim <56675100+NishanAsim@users.noreply.github.com>
2021-04-07 12:31:20 +0530

    Added example project

---
+++ AsyncSample/AsyncApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using FileParserLibrary;
+
+namespace AsyncApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            await FileParser.ReadFile(@"D:\Code\GitHubPublic\Training\AsyncSample\AsyncApp\Program.cs");
+            await FileParser.ReadFile(@"D:\Code\GitHubPublic\Training\AsyncSample\FileParserLibrary\FileParser.cs");
+
+            // var program = await FileParser.ReadFile(@"D:\Code\GitHubPublic\Training\AsyncSample\AsyncApp\Program.cs");
+            // var library = await FileParser.ReadFile(@"D:\Code\GitHubPublic\Training\AsyncSample\FileParserLibrary\FileParser.cs");
+            // await program;
+            // await library;
+            Console.WriteLine("Done");
+
+        }
+    }
+}
---
+++ AsyncSample/FileParserLibrary/FileParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileParserLibrary
+{
+    public class FileParser
+    {
+        public static async Task ReadFile(string filePath)
+        {
+            using StreamReader fs = File.OpenText(filePath);
+            byte[] b = new byte[100];
+            var line = await fs.ReadLineAsync();
+            while (line != null)
+            {
+               
+                Console.WriteLine(line);
+                line = await fs.ReadLineAsync();
+            }
+        }
+    }
+}
